575452a28f2073878e3892f2cde14ca12456b6ca
te25son <te25son@gmail.com>
2021-01-15 13:13:46 +0100

    Add get actions

--- StarChart/Controllers/CelestialObjectController.cs
+++ StarChart/Controllers/CelestialObjectController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.Linq;
+using Microsoft.AspNetCore.Mvc;
 using StarChart.Data;
 
 namespace StarChart.Controllers
@@ -12,6 +13,64 @@ namespace StarChart.Controllers
         public CelestialObjectController(ApplicationDbContext context)
         {
             _context = context;
+        }
+
+        [HttpGet("{id:int}"), ActionName("GetById")]
+        public IActionResult GetById(int id)
+        {
+            var celestialObject = _context.CelestialObjects
+                .Where(o => o.Id == id)
+                .FirstOrDefault();
+
+            if (celestialObject == null)
+                return NotFound();
+
+            var satellites = _context.CelestialObjects
+                .Where(o => o.OrbitedObjectId == id)
+                .ToList();
+
+            celestialObject.Satellites = satellites;
+            _context.SaveChanges();
+
+            return Ok(celestialObject);
+        }
+
+        [HttpGet("{name}")]
+        public IActionResult GetByName(string name)
+        {
+            var celestialObject = _context.CelestialObjects
+                .Where(o => o.Name == name)
+                .FirstOrDefault();
+
+            if (celestialObject == null)
+                return NotFound();
+
+            var satellites = _context.CelestialObjects
+                .Where(o => o.OrbitedObjectId == o.Id)
+                .ToList();
+
+            celestialObject.Satellites = satellites;
+            _context.SaveChanges();
+
+            return Ok(celestialObject);
+        }
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            var celestialObjects = _context.CelestialObjects.ToList();
+
+            foreach (var celestialObject in celestialObjects)
+            {
+                var satellites = _context.CelestialObjects
+                    .Where(o => o.OrbitedObjectId == o.Id)
+                    .ToList();
+
+                celestialObject.Satellites = satellites;
+                _context.SaveChanges();
+            }
+
+            return Ok(celestialObjects);
         }
     }
 }
